2d415b96f28441b89a6c9b0b420553dad521ca3f
“sawa” <silkyworkspace@gmail.com>
2025-08-17 16:02:23 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import { Plus, Minus, RotateCcw } from 'lucide-react';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // カウントアップ関数
+  const handleIncrement = () => {
+    //連続クリック時、まだ更新されていない古い値を使ってしまうので、
+    // 関数型更新が安全(Reactが「必ず最新の値」をprevCountに渡してくれる)
+    // setCount(count + 1); 
+    setCount(prevCount => prevCount + 1);
+  };
+  // カウントダウン関数
+  const handleDecrement = () => {
+    // setCount(count - 1);
+    setCount(prevCount => prevCount - 1);
+  };
+
+  // リセット関数
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <div className='flex items-center justify-center min-h-screen text-gray-300 bg-fancy'>
+        <div className='max-w-xs sm:max-w-lg'>
+          <h1 className='mb-6 text-xl font-medium md:text-2xl font-inter'>useState カウント</h1>
+
+          {/* カウントエリア */}
+          <p className='mb-4 font-light font-inter'>現在のカウント</p>
+          <p className='p-2 mb-10 text-3xl font-semibold tracking-wider border rounded md:w-48 md:mx-auto bg-white/10 font-fira'>{count}</p>
+
+          {/* ボタンエリア */}
+          <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+            <button onClick={handleIncrement} className='app_btn'>
+              <span className='btn_name'>Count Up</span>
+              <span className='inline-block'>
+                <Plus className='w-4 h-4' />
+              </span>
+            </button>
+            <button onClick={handleDecrement} className='app_btn'>
+              <span className='btn_name'>Count Down</span>
+              <span className='inline-block w-4'>
+                <Minus className='w-4 h-4' />
+              </span>
+            </button>
+            <button onClick={handleReset} className='app_btn'>
+              <span className='btn_name'>Reset</span>
+              <span className='inline-block w-4'>
+                <RotateCcw className='w-4 h-4' />
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
